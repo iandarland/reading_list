b65d1eb38115daa505c83baedb50af82fcf17041
Ian Darland <ianpdarland@gmail.com>
2022-04-27 22:35:39 -0500

    front and backend running concurrently

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context'
+import { Hello } from './pages/Hello'
 
 // Graphql API endpoint created
 const httpLink = createHttpLink({
@@ -31,22 +32,11 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client = {client}>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Hello/>}/>
+        </Routes>
+      </Router>
     </ApolloProvider>
   );
 }
